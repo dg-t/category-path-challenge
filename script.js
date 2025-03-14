a776ea578b681c8e3387aaced606116f132b4c56
Thomas Di Giovine <orione_90@hotmail.com>
2025-03-14 11:52:36 +0100

    Remove duplicate comment slashes in output samples

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ const getCategoryPath = (categories, categoryName) => {
   return null;
 };
 
-// // OUTPUT SAMPLES
+// OUTPUT SAMPLES
 console.log(getCategoryPath(categories, 'category4')); // should output: '/category1/category3/category4'
 console.log(getCategoryPath(categories, 'category2')); // should output: '/category1/category2'
 console.log(getCategoryPath(categories, 'category5')); // should output: '/category5'

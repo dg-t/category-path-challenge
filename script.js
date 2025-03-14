229ebc2ff78fe82f02b176d054d8b3ac6ec61620
Thomas Di Giovine <orione_90@hotmail.com>
2025-03-14 11:49:19 +0100

    Implement getCategoryPath function for finding category paths

--- script.js
+++ script.js
@@ -23,14 +23,20 @@ const categories = [
   },
 ];
 
-// TO-DO: Implement this function
 const getCategoryPath = (categories, categoryName) => {
-  let path;
+  for (const category of categories) {
+    if (category.name === categoryName) return `/${categoryName}`;
 
-  return path;
+    if (category.subcategories.length) {
+      const subcategory = getCategoryPath(category.subcategories, categoryName);
+      if (subcategory) return `/${category.name}${subcategory}`;
+    }
+  }
+
+  return null;
 };
 
-// OUTPUT SAMPLES
+// // OUTPUT SAMPLES
 console.log(getCategoryPath(categories, 'category4')); // should output: '/category1/category3/category4'
 console.log(getCategoryPath(categories, 'category2')); // should output: '/category1/category2'
 console.log(getCategoryPath(categories, 'category5')); // should output: '/category5'
